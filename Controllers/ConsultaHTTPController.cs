4357c27d062f4f062a5c7286c714f6aa067b03b9
MarcoAntonioFI <contato.marcoantonio99@gmail.com>
2025-08-20 14:04:35 -0300

    api-request-status-site
    0.1

---
+++ Controllers/ConsultaHTTPController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class  MonitoramentoController : ControllerBase
+{
+    private readonly IMonitoramentoService _monitoramentoService;
+
+    public MonitoramentoController(IMonitoramentoService monitoramentoService)
+    {
+        _monitoramentoService = monitoramentoService;
+    }
+
+    [HttpGet("verificar")]
+    public async Task<IActionResult> VerificarSiteAsync([FromQuery] string url)
+    {
+        if (string.IsNullOrWhiteSpace(url))
+        {
+            return BadRequest("URL não pode ser vazia.");
+        }
+        var resultado = await _monitoramentoService.VerificarSiteAsync(url);
+        if (resultado)
+        {
+            return Ok("Site está online.");
+        }
+        else
+        {
+            return StatusCode(503, "Site está offline ou inacessível.");
+        }
+    }
+}
+
---
+++ Services/ConsultaService.cs
@@ -0,0 +1,41 @@
+﻿public interface IMonitoramentoService
+{
+    Task<bool>
+        VerificarSiteAsync(string url);
+}
+
+public class MonitoramentoService : IMonitoramentoService
+{
+    private readonly HttpClient
+        _httpCliente;
+
+    public
+    MonitoramentoService(HttpClient httpClient)
+    {
+        _httpCliente = httpClient;
+        _httpCliente.Timeout = TimeSpan.FromSeconds(5);
+
+        _httpCliente.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "+"(KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36");
+    }
+       
+    public async Task<bool>VerificarSiteAsync(string url)
+    {
+        try
+        {
+            if (!url.StartsWith("http"))
+                url = "https://" + url;
+            var response = await _httpCliente.GetAsync(url);
+
+            return
+            ((int)response.StatusCode >= 200 &&
+            (int)response.StatusCode < 400);
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+
+
+}
